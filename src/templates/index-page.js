a4ccae58bb77897c5c7876d51fac2e099322c806
Charlie Depman <cdepman@gmail.com>
2021-01-09 21:35:53 -0800

    change ordering of elements on index page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -6,7 +6,7 @@ import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
-export const IndexPageTemplate = ({
+export const IndexPageTemplate =  ({
   image,
   title,
   heading,
